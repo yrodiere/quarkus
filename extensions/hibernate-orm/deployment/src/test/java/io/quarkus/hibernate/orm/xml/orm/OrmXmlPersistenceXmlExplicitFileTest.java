923cc35659b2b4b44242d77a5ac547af169fd0d9
Yoann Rodière <yoann@hibernate.org>
2021-03-18 16:57:46 +0100

    Test basic support of orm.xml
    Signed-off-by: Yoann Rodière <yoann@hibernate.org>

---
+++ extensions/hibernate-orm/deployment/src/test/java/io/quarkus/hibernate/orm/xml/orm/OrmXmlPersistenceXmlExplicitFileTest.java
@@ -0,0 +1,57 @@
+package io.quarkus.hibernate.orm.xml.orm;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.transaction.Transactional;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.hibernate.orm.SchemaUtil;
+import io.quarkus.hibernate.orm.SmokeTestUtils;
+import io.quarkus.test.QuarkusUnitTest;
+
+/**
+ * Test that assigning an orm.xml mapping file explicitly works as expected
+ * when configuring the persistence unit through persistence.xml.
+ */
+public class OrmXmlPersistenceXmlExplicitFileTest {
+
+    @RegisterExtension
+    static QuarkusUnitTest runner = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addClass(SmokeTestUtils.class)
+                    .addClass(SchemaUtil.class)
+                    .addClass(NonAnnotatedEntity.class)
+                    .addAsManifestResource("META-INF/explicitormxml.persistence.xml", "persistence.xml")
+                    .addAsManifestResource("META-INF/simple.orm.xml", "my-orm.xml")
+                    .addAsResource("application-datasource-only.properties"));
+
+    @Inject
+    EntityManagerFactory entityManagerFactory;
+
+    @Inject
+    EntityManager entityManager;
+
+    @Test
+    @Transactional
+    public void ormXmlTakenIntoAccount() {
+        assertThat(SchemaUtil.getColumnNames(entityManagerFactory, NonAnnotatedEntity.class))
+                .contains("thename")
+                .doesNotContain("name");
+    }
+
+    @Test
+    @Transactional
+    public void smokeTest() {
+        SmokeTestUtils.testSimplePersistRetrieveUpdateDelete(entityManager,
+                NonAnnotatedEntity.class, NonAnnotatedEntity::new,
+                NonAnnotatedEntity::getId, NonAnnotatedEntity::setName, NonAnnotatedEntity::getName);
+    }
+
+}
